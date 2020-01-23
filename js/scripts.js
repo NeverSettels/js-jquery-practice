9a3d191ba415aa5e63da2ce28b74d9f5c611ca9a
NeverSettels <settels.eric@gmail.com>
2020-01-22 20:35:49 -0800

    correct minor Jquery bug and collect data from inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
-$(document).ready(() => {
-    $("form1").submit(() => {
+$(document).ready(function () {
+    console.log('loaded')
+    $("#form1").submit(function (event) {
+
+
+        var q1 = $('#question1').val()
+        var q2 = $('#question2').val()
+        var q3 = $('#question3').val()
+        var q4 = $('#question4').val()
+        var q5 = $('#question1').val()
+
+        console.log(q1, q2, q3, q4, q5);
+
+
+
         event.preventDefault()
-
-
-
-
-
     })
 
 
