480bd967da3850c6f407993c4cc5c4e3d0e2fa92
NeverSettels <settels.eric@gmail.com>
2020-01-22 21:10:00 -0800

    added if statements to get the results of your quiz

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,29 @@ $(document).ready(function () {
     console.log('loaded')
     $("#form1").submit(function (event) {
 
-
-        var q1 = $('#question1').val()
-        var q2 = $('#question2').val()
-        var q3 = $('#question3').val()
-        var q4 = $('#question4').val()
-        var q5 = $('#question1').val()
+        var result = ''
+        var q1 = $('#question1').val().toLowerCase()
+        var q2 = $('#question2').val().toLowerCase()
+        var q3 = $('#question3').val().toLowerCase()
+        var q4 = $('#question4').val().toLowerCase()
+        var q5 = $('#question1').val().toLowerCase()
 
         console.log(q1, q2, q3, q4, q5);
 
+        if (q1 === "y" && q2 === "y" && q3 === "n" && q4 === "y") {
+            result = "You'll really enjoy JavaScript!"
+        }
+        else if (q1 === "n" && q2 === "n" && q3 === "y" && q4 === "y" || q4 === 'n') {
+            result = "If you're designed minded you'll probably enjoy CSS"
+        }
+        else if (q1 === "y" && q2 === "y" && q3 === "y" && q4 === "y" || q4 === 'n') {
+            result = "You like a bit of everything! That's Awesome! You'll really like messing around in React!"
+        }
+        else {
+            result = "You really dont seem to like any of them, maybe something other than programing is your thing! Also you might have entered something other than y or n"
+        }
 
-
+        console.log(result);
         event.preventDefault()
     })
 
