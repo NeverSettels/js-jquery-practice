885e7519ed082677e953bf69ff268b1167e6af03
NeverSettels <settels.eric@gmail.com>
2020-01-23 20:13:24 -0800

    Add error message in red in case of inputing something invalid and the logic to desplay it correctly

--- js/scripts.js
+++ js/scripts.js
@@ -10,20 +10,26 @@ $(document).ready(function () {
         var q5 = $('#question1').val().toLowerCase()
 
         console.log(q1, q2, q3, q4, q5);
-
-        if (q1 === "y" && q2 === "y" && q3 === "n" && q4 === "y") {
-            result = "You'll really enjoy JavaScript!"
-        }
-        else if (q1 === "n" && q2 === "n" && q3 === "y" && q4 === "y" || q4 === 'n') {
-            result = "If you're designed minded you'll probably enjoy CSS"
-        }
-        else if (q1 === "y" && q2 === "y" && q3 === "y" && q4 === "y" || q4 === 'n') {
-            result = "You like a bit of everything! That's Awesome! You'll really like messing around in React!"
+        const answered = q1.length + q2.length + q3.length + q4.length + q5.length
+        if (answered !== 5) {
+            result = 'Oops! Looks like you either forgot an answer or inputed something other than Y or N!'
+            $('#result').addClass('error')
         }
         else {
-            result = "You really dont seem to like any of them, maybe something other than programing is your thing! Also you might have entered something other than y or n"
-        }
+            if (q1 === "y" && q2 === "y" && q3 === "n" && q4 === "y") {
+                result = "You'll really enjoy JavaScript!"
+            }
+            else if (q1 === "n" && q2 === "n" && q3 === "y" && (q4 === "y" || q4 === 'n')) {
+                result = "If you're designed minded you'll probably enjoy CSS"
+            }
+            else if (q1 === "y" && q2 === "y" && q3 === "y" && q4 === "y" || q4 === 'n') {
+                result = "You like a bit of everything! That's Awesome! You'll really like messing around in React!"
+            }
+            else {
+                result = "You really dont seem to like any of them, maybe something other than programing is your thing!"
 
+            }
+        }
         $('#jumbo1').addClass('noDisplay')
         $('#jumbo2').removeClass('noDisplay')
         $("#result").text(result)
