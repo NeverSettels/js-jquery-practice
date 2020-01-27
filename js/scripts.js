09e278d4afd560c305cc440f6b41f5fc7b6d3543
NeverSettels <settels.eric@gmail.com>
2020-01-26 20:44:58 -0800

    Fix error in logic separation erase comment lines and clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,33 @@
 //Backend Logic
-function setResult(answered) {
+function setResult(q1, q2, q3, q4, q5) {
+    const answered = q1.length + q2.length + q3.length + q4.length + q5.length
+    let result = '';
+    let error = true;
     if (answered !== 5) {
-        return 'Oops! Looks like you either forgot an answer!'
+        result = 'Oops! Looks like you either forgot an answer!'
     }
     else if ((q1 != 'y' && q1 != "n") || (q2 != 'y' && q2 != "n") || (q3 != 'y' && q3 != "n") || (q4 != 'y' && q4 != "n") || (q5 != 'y' && q5 != "n")) {
-        return 'Oops! Looks like you inputed something other than Y or N!'
+        result = 'Oops! Looks like you inputed something other than Y or N!'
     }
     else {
-        $('#result').removeClass('error')
+        error = false
         if (q1 === "y" && q2 === "y" && q3 === "n" && q4 === "y") {
-            return "You'll really enjoy JavaScript!"
+            result = "You'll really enjoy JavaScript!"
         }
         else if (q1 === "n" && q2 === "n" && q3 === "y" && (q4 === "y" || q4 === 'n')) {
-            return "If you're designed minded you'll probably enjoy CSS"
+            result = "If you're designed minded you'll probably enjoy CSS"
         }
         else if (q1 === "y" && q2 === "y" && q3 === "y" && (q4 === "y" || q4 === 'n')) {
-            return "You like a bit of everything! That's Awesome! You'll really like messing around in React!"
+            result = "You like a bit of everything! That's Awesome! You'll really like messing around in React!"
         }
         else if (q1 === "n" && q2 === "n" && q3 === "n" && q4 === "n" && q5 === 'y') {
-            return "You'll probably enjoy just about any programing language! They're all about a whole lot of things coming together! "
+            result = "You'll probably enjoy just about any programing language! They're all about a whole lot of things coming together! "
         }
         else {
-            return "You really dont seem to like any of them, maybe something other than programing is your thing!"
+            result = "You really dont seem to like any of them, maybe something other than programing is your thing!"
         }
     }
+    return [result, error]
 }
 
 // Frontend logic
@@ -35,12 +39,13 @@ $(document).ready(function () {
         var q3 = $('#question3').val().toLowerCase()
         var q4 = $('#question4').val().toLowerCase()
         var q5 = $('#question5').val().toLowerCase()
-        const answered = q1.length + q2.length + q3.length + q4.length + q5.length
-        var result = setResult(answered)
 
+        if (!setResult(q1, q2, q3, q4, q5)[1]) {
+            $('#result').removeClass('error')
+        }
         $('#jumbo1').addClass('noDisplay')
         $('#jumbo2').removeClass('noDisplay')
-        $("#result").text(result)
+        $("#result").text(setResult(q1, q2, q3, q4, q5)[0])
     })
     $("#resetBtn").click(function () {
         location.reload(true);
